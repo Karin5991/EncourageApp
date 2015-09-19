2c892dc272942376169ff4b070ac4932d6487c3a
Karen Johnson <karin5991@Karen-Johnsons-MacBook-Pro.local>
2015-09-19 09:09:09 -0700

    added new files

--- js/encourage.js
+++ js/encourage.js
@@ -68,13 +68,7 @@ quote_container.innerHTML += '<i>' + quotes[randomQuote] + '</i><br>';
 
 
 
-    function goToNewPage()
-    {
-        var url = document.getElementById('list').value;
-        if(url != 'none') {
-            window.location = url;
-        }
-    }
+   
 
 
 
